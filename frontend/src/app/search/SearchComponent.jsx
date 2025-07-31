41e362d15d96a51a090f108e100568a09792da18
Himanshu Yadav <imhimanshu1605@gmail.com>
2025-08-01 04:36:16 +0530

    Update SearchComponent.jsx

--- frontend/src/app/search/SearchComponent.jsx
+++ frontend/src/app/search/SearchComponent.jsx
@@ -14,7 +14,7 @@ export default function SearchPage() {
     const fetchDoctors = async () => {
       try {
         const response = await fetch(
-          `https://my-backend10-780g.onrender.com/api/doctors?location=${location}&profession=${profession}`
+          `https://my-backend10.onrender.com/api/doctors?location=${location}&profession=${profession}`
         );
         const data = await response.json();
         setDoctors(data);
