7bd094ad6830b135a1357e161e205b74fc4751e9
Himanshu Yadav <imhimanshu1605@gmail.com>
2025-08-01 02:42:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
@@ -8,10 +8,23 @@ dotenv.config();
 const app = express();
 connectDB();
 
+const allowedOrigins = [
+  'https://my-frontend3.vercel.app',
+  'https://my-frontend3-git-main-himanshu-yadavs-projects-60edff19.vercel.app',
+  'https://my-frontend3-jo1l9kguh-himanshu-yadavs-projects-60edff19.vercel.app'
+];
+
 app.use(cors({
-  origin: "https://my-frontend3.vercel.app", // allow frontend origin
-  credentials: true // if you're using cookies or auth
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS error: Origin not allowed'));
+    }
+  },
+  credentials: true
 }));
+
 app.use(express.json());
 
 app.use('/api/doctors', doctorRoutes);
